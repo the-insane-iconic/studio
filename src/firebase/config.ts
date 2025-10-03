8edc63b91b3967da0e912bfc568da9d03773d008
Anupam Yadav <anupamyadav6477@gmail.com>
2025-10-03 21:29:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5359505589-304d1",
+  "appId": "1:751604533397:web:46c0d55d6b64317b0e739c",
+  "apiKey": "AIzaSyAXHUOO9kpsI_GL4IgAzKE1ryHfcD9M5Ns",
+  "authDomain": "studio-5359505589-304d1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "751604533397"
+};
